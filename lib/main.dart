afbbaebfb978e442b5fb8f9b9fc10493c0d9bd0c
mdava3776 <mdava3776@gmail.com>
2026-02-12 10:38:39 +0000

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hiyo Ganteng lmao'),
     );
   }
 }
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('You pushed the buttons for: '),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
